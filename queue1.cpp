edc260703385bdc5bfa863d4c14a1a9834ddd76f
scoupse-svt <maksumovaina@gmail.com>
2026-04-22 11:54:53 +0400

    Базовые ф-ции для работы с очередью

---
+++ queue1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+struct queue {
+    int inf; //хранится элемент, который мы положили в очередь
+    queue* next;//указатель на след. элемент
+};
+
+void push(queue*& h, queue*& t, int x) { //ф-ция добавления элементов в хвост
+    queue* r = new queue;
+    r->inf = x;
+    r->next = NULL;
+
+    if (!h && !t) {
+        h = t = r;
+    }
+    else {
+        t->next = r;
+        t = r;
+    }
+}
+
+int pop(queue*& h, queue*& t) { //ф-ция удаления из головы
+    if (!h) return -1; //в случае пустой очереди
+
+    queue* r = h;
+    int val = h->inf;
+    h = h->next;
+
+//    if (!h) t = NULL;
+    delete r;
+    return val;
+}
+
+void print(queue* h, queue* t) {
+    queue* tmph = NULL;
+    queue* tmpt = NULL;
+    while (h) {
+        int x = pop(h, t);
+        cout << x << " ";
+        push(tmph, tmpt, x);
+    }
+    cout << endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    queue* h = NULL;
+    queue* t = NULL;
+
+    int n;
+    cout << "Введите количество элементов: ";
+    cin >> n;
+    cin.ignore(10000, '\n');
+
+    cout << "Введите " << n << " целых чисел через пробел: ";
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        push(h, t, x);
+    }
+}
