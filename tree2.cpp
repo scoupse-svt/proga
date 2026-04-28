64ac90ba4326b8b3a9cea21ef17181b3fd98127f
scoupse-svt <maksumovaina@gmail.com>
2026-04-28 21:43:38 +0400

    Базовые ф-ции для работы с деревьями

---
+++ tree2.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+struct tree {
+    int inf;
+    tree* lt; //левый ребёнок
+    tree* rt; //правый ребёнок
+    tree* pt; // родитель
+};
+
+tree* node(int x) { // начальный узел
+    tree* n = new tree;
+    n->inf = x;
+    n->lt = n->rt = NULL;
+    n->pt = NULL;
+    return n;
+}
+
+void insert(tree*& tr, int x) { //вставка
+    tree* n = node(x);
+    if (!tr) tr = n; //если дерево пустое, то это корень
+    else {
+        tree* y = tr;
+        while (y) {
+            if (n->inf > y->inf) { //правая ветка
+                if (y->rt) y = y->rt;
+                else {
+                    n->pt = y; //узел становится правым ребёнком
+                    y->rt = n;
+                    break;
+                }
+            }
+            else if (n->inf < y->inf) { //левая ветка
+                if (y->lt) y = y->lt;
+                else {
+                    n->pt = y; //узел становится левым ребёнком
+                    y->lt = n;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+tree* find(tree* tr, int x) {
+    if (!tr || tr->inf == x) return tr; //если нашли или дошли до конца
+    if (x < tr->inf) {
+        return find(tr->lt, x);
+    }
+    else {
+        return find(tr->rt, x);
+    }
+}
+
+void inorder(tree* tr) { //симметричный обход для вывода
+    if (tr) {
+        inorder(tr->lt);
+        cout << tr->inf << " ";
+        inorder(tr->rt);
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+    setlocale(LC_ALL, "");
+
+    tree* root = NULL;
+
+    int n = 0;
+    int x = 0;
+    cout << "Введите количество элементов: ";
+    cin >> n;
+    cout << "Введите элементы: ";
+    for (int i = 0; i < n; i++) {
+        int t = 0;
+        cin >> t;
+        insert(root, t);
+    }
+
+    cout << "Текущее дерево (симметричный обход): ";
+    inorder(root);
+    cout << endl;
+}
