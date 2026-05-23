7cf625a8088eb0f6af2e68ba9ac47e87e7b455c6
scoupse-svt <maksumovaina@gmail.com>
2026-05-23 21:09:26 +0400

    Добавлены функции для балансировки дерева

--- tree4.cpp
+++ tree4.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 struct tree {
     int inf;
+    int height;
     tree* lt;
     tree* rt;
 };
@@ -10,23 +11,80 @@ struct tree {
 tree* node(int x) {
     tree* n = new tree;
     n->inf = x;
+    n->height = 1;
     n->lt = n->rt = NULL;
     return n;
 }
 
-//сбалансированное дерево
-void balanced(tree*& tr, int* arr, int start, int end) {
-    if (start > end) {
-        tr = NULL;
-        return;
-    }
-    int mid = (start + end) / 2;
-    tr = node(arr[mid]);
-    balanced(tr->lt, arr, start, mid - 1);
-    balanced(tr->rt, arr, mid + 1, end);
+//функции для балансировки
+int height(tree* tr) {
+    return tr ? tr->height : 0;
 }
 
-//симметричный обход
+void updateHeight(tree* tr) {
+    if (tr) {
+        int hl = height(tr->lt);
+        int hr = height(tr->rt);
+        tr->height = (hl > hr ? hl : hr) + 1;
+    }
+}
+
+int balanceFactor(tree* tr) {
+    return tr ? height(tr->lt) - height(tr->rt) : 0;
+}
+
+tree* rotateRight(tree* y) {
+    tree* x = y->lt;
+    tree* T2 = x->rt;
+    x->rt = y;
+    y->lt = T2;
+    updateHeight(y);
+    updateHeight(x);
+    return x;
+}
+
+tree* rotateLeft(tree* x) {
+    tree* y = x->rt;
+    tree* T2 = y->lt;
+    y->lt = x;
+    x->rt = T2;
+    updateHeight(x);
+    updateHeight(y);
+    return y;
+}
+
+tree* balance(tree* tr) {
+    if (!tr) return tr;
+    updateHeight(tr);
+    int bf = balanceFactor(tr);
+    // левый перекос
+    if (bf > 1) {
+        if (balanceFactor(tr->lt) < 0)
+            tr->lt = rotateLeft(tr->lt);
+        return rotateRight(tr);
+    }
+    // правый перекос
+    if (bf < -1) {
+        if (balanceFactor(tr->rt) > 0)
+            tr->rt = rotateRight(tr->rt);
+        return rotateLeft(tr);
+    }
+    return tr;
+}
+
+//вставка в дерево
+tree* insertAVL(tree* tr, int x) {
+    if (!tr) return node(x);
+    if (x < tr->inf)
+        tr->lt = insertAVL(tr->lt, x);
+    else if (x > tr->inf)
+        tr->rt = insertAVL(tr->rt, x);
+    else
+        return tr; // дубликаты не вставляем
+    return balance(tr);
+}
+
+// симметричный обход
 void inorder(tree* tr) {
     if (tr) {
         inorder(tr->lt);
@@ -43,37 +101,20 @@ int leafsum(tree* tr) {
 }
 
 int main() {
-    setlocale(LC_ALL, "");
     tree* root = NULL;
+    int n, x;
 
-    int n;
     cout << "Введите количество элементов: ";
     cin >> n;
-
-    int* arr = new int[n];
     cout << "Введите элементы: ";
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        cin >> x;
+        root = insertAVL(root, x);
     }
 
-    // Сортируем массив, чтобы дерево было сбалансированным
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (arr[i] > arr[j]) {
-                int tmp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tmp;
-            }
-        }
-    }
-
-    balanced(root, arr, 0, n - 1);
-    delete[] arr;
-
-    cout << "\nСимметричный обход: ";
+    cout << "\nСимметричный обход AVL-дерева: ";
     inorder(root);
     cout << endl;
 
-    int sum = leafsum(root);
-    cout << "Сумма листьев: " << sum << endl;
+    cout << "Сумма листьев: " << leafsum(root) << endl;
 }
